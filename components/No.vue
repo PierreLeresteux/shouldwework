44c262f12b0e135b1f65e65e674b0eac8431a4dc
Pierre Leresteux <pierre.leresteux@gmail.com>
2021-01-21 09:42:10 +0100

    Add even/odd

---
+++ components/No.vue
@@ -0,0 +1,30 @@
+<template>
+    <h2 class="subtitle">
+        The answer is NO
+        <div v-if="odd">Because it's an odd day !</div>
+        <div v-else="odd">Because it's an even day !</div>
+    </h2>
+</template>
+
+<style>
+    .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+    }
+
+</style>
+
+<script>
+export default {
+  data() {
+        var day = new Date().getDate();
+        return {
+            odd: day % 2 != 0
+        }
+    },
+    
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,7 @@
     <div>
       <logo />
       <question />
-      <h2 class="subtitle">
-        The answer is NO
-      </h2>
+      <no />
     </div>
   </div>
 </template>
@@ -13,11 +11,13 @@
 <script>
 import Logo from '~/components/Logo.vue'
 import Question from '~/components/Question.vue'
+import No from '~/components/No.vue'
 
 export default {
   components: {
     Logo,
-    Question
+    Question,
+    No,
   }
 }
 </script>
@@ -30,16 +30,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
 }
 
 </style>
